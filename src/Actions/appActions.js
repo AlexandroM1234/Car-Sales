13c1c3a83cef0b7b0738f9990240caaa0c947ae7
Alex Martinez <alexmartinez0071234@gmail.com>
2020-03-17 16:59:47 -0400

    actions made

---
+++ src/Actions/appActions.js
@@ -0,0 +1,17 @@
+export const ADD_FEATURE = "ADD_FEATURE";
+
+export const REMOVE_FEATURES = "REMOVE_FEATURES";
+
+export const UPDATE_TOTAL = "UPDATE_TOTAL";
+
+export const addFeature = () => {
+  return { type: ADD_FEATURE };
+};
+
+export const removeFeatures = () => {
+  return { type: REMOVE_FEATURES };
+};
+
+export const updateTotal = () => {
+  return { type: UPDATE_TOTAL };
+};
